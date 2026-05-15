eefb2f942ceee3249221aafe702157712cb5615e
Rahul-Anthony <rahulcoding23@gmail.com>
2026-05-15 17:08:28 +0530

    first updata

--- Hrmanagement/src/App.jsx
+++ Hrmanagement/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Dashboard from './dashboard'
+//import Dashboard from './dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Payroll</h1>
     </>
   )
 }
