465d01dcd7fae650bbec3f63d1510c239d03770b
akaravinth524-dotcom <akaravinth524@gmail.com>
2026-05-25 11:56:31 +0530

    Changed

--- Hrmanagement/src/App.jsx
+++ Hrmanagement/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Dashboard from './dashboard'
+import Employee from './Employee'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Employee></Employee>
     </>
   )
 }
---
+++ Hrmanagement/src/Employee.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Employee() {
+  return (
+    <h1>AK</h1>
+  )
+}
+
+export default Employee
