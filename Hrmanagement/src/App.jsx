d13f3663f5c4bac43ba6f66baeb065b98667962a
KJR577 <kenvin577@gmail.com>
2026-05-28 11:45:50 +0530

    new files added

--- Hrmanagement/src/App.jsx
+++ Hrmanagement/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Dashboard from './dashboard'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import LoginInfo from './pages/LoginInfo';
+import PlaceholderPage from './pages/PlaceholderPage';
+import './styles/global.css';
+import Dashboard from './Dashboard/Dashboard';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <Dashboard />
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        {/* Redirect root to dashboard */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-export default App
+        {/* All pages share the sidebar Layout */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard"   element={<Dashboard/>} />
+          <Route path="/employee"    element={<PlaceholderPage />} />
+          <Route path="/attendance"  element={<PlaceholderPage />} />
+          <Route path="/payroll"     element={<PlaceholderPage />} />
+          <Route path="/leave"       element={<PlaceholderPage />} />
+          <Route path="/recruitment" element={<PlaceholderPage />} />
+          <Route path="/login-info"  element={<LoginInfo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
